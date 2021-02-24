4ade0fdd36f7cebb88ee02eedcafb578fc11564f
rxgvnd <ira.suryani@student.umn.ac.id>
2021-02-24 11:39:31 +0800

    Commit Week 4

---
+++ Week04A_31410/app/src/main/java/umn/ac/id/week04a_31410/MainActivity.java
@@ -0,0 +1,62 @@
+package umn.ac.id.week04a_31410;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etIsian, etUrl;
+    private Button btnKirim, btnBrowse;
+    private TextView tvJawaban;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etIsian = findViewById(R.id.isian);
+        etUrl = findViewById(R.id.url);
+        btnBrowse = findViewById(R.id.buttonBrowse);
+        btnKirim = findViewById(R.id.buttonKirim);
+        tvJawaban = findViewById(R.id.jawaban);
+
+        btnBrowse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String urlText = etUrl.getText().toString();
+                if (urlText.isEmpty()) {
+                    urlText = "http://www.umn.ac.id/";
+                }
+                Intent browseIntent = new Intent(Intent.ACTION_VIEW);
+                browseIntent.setData(Uri.parse(urlText));
+                if (browseIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(browseIntent);
+                }
+            }
+        });
+
+        btnKirim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentDua = new Intent(MainActivity.this,
+                        ActivityDua.class);
+                String isian = etIsian.getText().toString();
+                intentDua.putExtra("PesanDariMain",isian);
+                startActivityForResult(intentDua, 1);
+            }
+        });
+    }
+
+    protected void onActivityResult (int requestCode, int resultCode,Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1){
+            String jawaban = data.getStringExtra("Jawaban");
+            tvJawaban.setText(jawaban);
+        }
+    }
+}
