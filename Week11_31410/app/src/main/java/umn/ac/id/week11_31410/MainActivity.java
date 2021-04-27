33fb0bffb000996ac61713f836eff84363021075
rxgvnd <ira.suryani@student.umn.ac.id>
2021-04-27 22:01:55 +0800

    Week 11 - Done

--- Week11_31410/app/src/main/java/umn/ac/id/week11_31410/MainActivity.java
+++ Week11_31410/app/src/main/java/umn/ac/id/week11_31410/MainActivity.java
@@ -1,14 +1,47 @@
 package umn.ac.id.week11_31410;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    Services services;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        services = APIClient.getClient().create(Services.class);
+        setup();
+    }
+
+    private void setup() {
+        Call<ArrayList<Model>> callPost = services.getPost();
+        callPost.enqueue(new Callback<ArrayList<Model>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Model>> call, Response<ArrayList<Model>> response) {
+                ArrayList<Model> hasilPost = response.body();
+                recyclerView.setAdapter(new Adapter(hasilPost));
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Model>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Gagal!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
